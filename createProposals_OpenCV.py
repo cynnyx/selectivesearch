e870eb31e46bac5a219cfbff5f7e821413af576f
Michele Toni <michele.toni@cynny.com>
2018-06-13 12:19:25 +0200

    Support for index file export

--- createProposals_OpenCV.py
+++ createProposals_OpenCV.py
@@ -8,7 +8,7 @@ import os
 from tqdm import tqdm
 import numpy as np
 
-from common import ImageProposals, prepareJSON, exportCrop
+from common import ImageProposals, resizeImage
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 from proposals.Proposal import Proposal
@@ -24,13 +24,16 @@ def do_parsing():
     parser.add_argument('--quality', action="store_true", help='More quality (recall), but slower')
     # Index format, single file
     parser.add_argument('--outputIndexFile', type=str, required=False, help='Output index file with proposals')
+    parser.add_argument('-r', '--relativeCoord', action='store_true',
+                        help='Use relative coordinates instead of absolute pixels for index file export')
     # Object detection proposals format, one JSON for each image
     parser.add_argument('--outputDirJSON', type=str, required=False, help='Output dir where to put JSON files')
     # Over 1000 regions the crops seem to be very similar
     parser.add_argument('--maxRegions', type=int, required=False, default=1000,
                         help='Maximum number of regions to export')
-    parser.add_argument('--minSize', type=int, required=False, default=32,
-                        help='Minimum image side (pixels) dimension to save the file crop')
+    parser.add_argument('--minSize', type=int, required=False, default=20,
+                        help='Minimum image side (pixels) dimension to save the file crop.'
+                             'Pixels are computed on the resized image, refers to maxSide parameter.')
     return parser.parse_args()
 
 
@@ -45,6 +48,13 @@ if __name__ == '__main__':
 
     # create Selective Search Segmentation Object using default parameters
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+
+    if args.outputIndexFile:
+        # TODO: Check existence and raise error, because we append
+        pass
+
+    # TODO: Iterate over one level of directories
+    subset = ""
 
     #Files to analyze, ordered by name
     imgFormats = ["jpg", "png", "jpeg"]
@@ -62,6 +72,9 @@ if __name__ == '__main__':
             # resize image, REALLY REALLY IMPORTANT FOR PERFORMANCES
             img_resized = resizeImage(img, width=width, height=height, maxSide=args.maxSide)
 
+            resized_width = img_resized.shape[1]
+            resized_height = img_resized.shape[0]
+
             # set input image on which we will run segmentation
             ss.setBaseImage(img_resized)
 
@@ -77,14 +90,50 @@ if __name__ == '__main__':
             regions = ss.process()
             print('Total Number of Region Proposals: {}'.format(len(regions)))
 
-            # FIXME: Permit fast Vs slow selection
-
             # TODO: Manage new outputs format, probably they are the same as before (array of cv2 rects as numpy array)
 
-            # TODO: Export crops in our index format without doing them
+            if args.outputIndexFile:
 
-            # region x, y, w, h
-            #img_lbl, regions = ss.selective_search(img, scale=500, sigma=0.9, min_size=10)
+                with open(args.outputIndexFile, 'a') as outputIndexFile:
+
+                    rects_dict = []
+
+                    for boundingBox in regions[:args.maxRegions]:
+
+                        x, y, w, h = boundingBox
+
+                        if w >= args.minSize and h >= args.minSize:
+
+                            this_rect_dict = dict()
+
+                            if args.relativeCoord:
+                                XMinR = float(x) / float(resized_width - 1)
+                                YMinR = float(y) / float(resized_height - 1)
+                                widthR = float(w) / float(resized_width - 1)
+                                heightR = float(h) / float(resized_height - 1)
+
+                                this_rect_dict["x_rel"] = XMinR
+                                this_rect_dict["y_rel"] = YMinR
+                                this_rect_dict["width_rel"] = widthR
+                                this_rect_dict["height_rel"] = heightR
+                            else:
+                                # Retrieve absolute positions on the original image
+                                x_original = int(x * width / resized_width)
+                                y_original = int(y * height / resized_height)
+                                w_original = int(w * width / resized_width)
+                                h_original = int(h * height / resized_height)
+
+                                this_rect_dict["x"] = x_original
+                                this_rect_dict["y"] = y_original
+                                this_rect_dict["width"] = w_original
+                                this_rect_dict["height"] = h_original
+
+                            rects_dict.append(this_rect_dict)
+
+                    rects_dict_string = json.dumps(rects_dict)
+
+                    outputIndexFile.writelines([os.path.join(subset, os.path.basename(imageFile)),
+                                                "\t", rects_dict_string, "\n"])
 
             # Proposals export in JSON for our object detection training pipeline
             if args.outputDirJSON is not None:
@@ -92,7 +141,7 @@ if __name__ == '__main__':
                 if os.path.exists(args.outputDirJSON) is False:
                     os.makedirs(args.outputDirJSON)
 
-                proposals = prepareJSON(width, height, regions, maxRegions=args.maxRegions)
+                #proposals = prepareJSON(width, height, regions, maxRegions=args.maxRegions)
                 # {
                 # "image": "000001",
                 # "proposals": [
@@ -102,9 +151,9 @@ if __name__ == '__main__':
                 #        "x": 0.0906515568494797,
                 #        "y": 0.0
                 #    },
-                imageProposal = ImageProposals(imageName=imageNameWithoutExtension, proposals=proposals)
-                with open(os.path.join(args.outputDirJSON, imageNameWithoutExtension + ".json"), 'w') as outfile:
-                    json.dump(imageProposal, outfile, default=lambda o: o.__dict__, indent=4, sort_keys=True)
+                #imageProposal = ImageProposals(imageName=imageNameWithoutExtension, proposals=proposals)
+                #with open(os.path.join(args.outputDirJSON, imageNameWithoutExtension + ".json"), 'w') as outfile:
+                #    json.dump(imageProposal, outfile, default=lambda o: o.__dict__, indent=4, sort_keys=True)
 
             # Proposals crops export as images
             if args.outputDirCrop is not None:
@@ -112,88 +161,5 @@ if __name__ == '__main__':
                 if os.path.exists(args.outputDirCrop) is False:
                     os.makedirs(args.outputDirCrop)
 
-                exportCrop(img, regions, args.outputDirCrop, imageNameWithoutExtension,
-                           minSize=args.minSize, maxRegions=args.maxRegions)
-
-# if __name__ == '__main__':
-#
-#     args = do_parsing()
-#     print(args)
-#
-#     # If image path and f/q is not passed as command
-#     # line arguments, quit and display help message
-#     if len(sys.argv) < 3:
-#         print(__doc__)
-#         sys.exit(1)
-#
-#     # speed-up using multithreads
-#     cv2.setUseOptimized(True)
-#     cv2.setNumThreads(4)
-#
-#     # read image
-#     im = cv2.imread(sys.argv[1])
-#     # resize image
-#     newHeight = 200
-#     newWidth = int(im.shape[1]*200/im.shape[0])
-#     im = cv2.resize(im, (newWidth, newHeight))
-#
-#     # create Selective Search Segmentation Object using default parameters
-#     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
-#
-#     # set input image on which we will run segmentation
-#     ss.setBaseImage(im)
-#
-#     # Switch to fast but low recall Selective Search method
-#     if (sys.argv[2] == 'f'):
-#         ss.switchToSelectiveSearchFast()
-#
-#     # Switch to high recall but slow Selective Search method
-#     elif (sys.argv[2] == 'q'):
-#         ss.switchToSelectiveSearchQuality()
-#     # if argument is neither f nor q print help message
-#     else:
-#         print(__doc__)
-#         sys.exit(1)
-#
-#     # run selective search segmentation on input image
-#     rects = ss.process()
-#     print('Total Number of Region Proposals: {}'.format(len(rects)))
-#
-#     # number of region proposals to show
-#     numShowRects = 100
-#     # increment to increase/decrease total number
-#     # of reason proposals to be shown
-#     increment = 50
-#
-#     while True:
-#         # create a copy of original image
-#         imOut = im.copy()
-#
-#         # itereate over all the region proposals
-#         for i, rect in enumerate(rects):
-#             # draw rectangle for region proposal till numShowRects
-#             if (i < numShowRects):
-#                 x, y, w, h = rect
-#                 cv2.rectangle(imOut, (x, y), (x+w, y+h), (0, 255, 0), 1, cv2.LINE_AA)
-#             else:
-#                 break
-#
-#         # show output
-#         cv2.imshow("Output", imOut)
-#
-#         # record key press
-#         k = cv2.waitKey(0) & 0xFF
-#
-#         # m is pressed
-#         if k == 109:
-#             # increase total number of rectangles to show by increment
-#             numShowRects += increment
-#         # l is pressed
-#         elif k == 108 and numShowRects > increment:
-#             # decrease total number of rectangles to show by increment
-#             numShowRects -= increment
-#         # q is pressed
-#         elif k == 113:
-#             break
-#     # close image show window
-#     cv2.destroyAllWindows()
+                #exportCrop(img, regions, args.outputDirCrop, imageNameWithoutExtension,
+                #           minSize=args.minSize, maxRegions=args.maxRegions)
