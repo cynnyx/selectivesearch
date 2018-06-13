bc5bac8bf2e964519057bb950849757ca58dc201
Michele Toni <michele.toni@cynny.com>
2018-06-13 15:27:12 +0200

    Support all exports and minor refactoring

--- createProposals_OpenCV.py
+++ createProposals_OpenCV.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 from tqdm import tqdm
-import numpy as np
+import skimage.io
 
 from common import ImageProposals, resizeImage
 
@@ -20,14 +20,16 @@ def do_parsing():
         description='Script to export selective search proposals')
     parser.add_argument('--imagesDir', type=str, required=True, help='Folder containing input images')
     parser.add_argument('--maxSide', type=int, required=False, default=300, help='Max side of image preprocess resize')
-    parser.add_argument('--outputDirCrop', type=str, required=False, help='Output dir where to put images crops')
     parser.add_argument('--quality', action="store_true", help='More quality (recall), but slower')
     # Index format, single file
     parser.add_argument('--outputIndexFile', type=str, required=False, help='Output index file with proposals')
-    parser.add_argument('-r', '--relativeCoord', action='store_true',
+    parser.add_argument('-r', '--relativeCoordIndexFile', action='store_true',
                         help='Use relative coordinates instead of absolute pixels for index file export')
     # Object detection proposals format, one JSON for each image
-    parser.add_argument('--outputDirJSON', type=str, required=False, help='Output dir where to put JSON files')
+    parser.add_argument('--outputDirJson', type=str, required=False,
+                        help='Output dir where to put JSON files '
+                             '(serialization of proposals objects with relative coordinates')
+    parser.add_argument('--outputDirCrop', type=str, required=False, help='Output dir where to put images crops')
     # Over 1000 regions the crops seem to be very similar
     parser.add_argument('--maxRegions', type=int, required=False, default=1000,
                         help='Maximum number of regions to export')
@@ -35,6 +37,52 @@ def do_parsing():
                         help='Minimum image side (pixels) dimension to save the file crop.'
                              'Pixels are computed on the resized image, refers to maxSide parameter.')
     return parser.parse_args()
+
+
+def writeIndexFile(imageRelPath, outputIndexFilePath, rects_dict):
+
+    with open(outputIndexFilePath, 'a') as outputIndexFile:
+
+        rects_dict_string = json.dumps(rects_dict)
+
+        outputIndexFile.writelines([imageRelPath, "\t", rects_dict_string, "\n"])
+
+
+def prepareJSON(imageRelPath, rects_dict, outputDirJson):
+
+    proposals = []
+
+    for rect_dict in tqdm(rects_dict, desc="JSON"):
+
+        relX = rect_dict["x_rel"]
+        relY = rect_dict["y_rel"]
+        relW = rect_dict["width_rel"]
+        relH = rect_dict["height_rel"]
+
+        assert relW <= 1.0, relW >= 0.0
+        assert relH <= 1.0, relH >= 0.0
+
+        proposal = Proposal(relX, relY, relW, relH)
+        proposals.append(proposal)
+
+    imageNameWithoutExtension = imageRelPath[:imageFile.rindex('.')]
+    imageProposal = ImageProposals(imageName=imageNameWithoutExtension, proposals=proposals)
+    with open(os.path.join(outputDirJson, imageNameWithoutExtension + ".json"), 'w') as outfile:
+        json.dump(imageProposal, outfile, default=lambda o: o.__dict__, indent=4, sort_keys=True)
+
+
+def exportCrop(imageRelPath, image, rects_dict, outputDirCrop):
+
+    for index, rect_dict in tqdm(enumerate(rects_dict), desc="Crop"):
+
+        x = rect_dict["x"]
+        y = rect_dict["y"]
+        w = rect_dict["width"]
+        h = rect_dict["height"]
+
+        crop = image[y:y+h, x:x+w]
+        imageNameWithoutExtension = imageRelPath[:imageFile.rindex('.')]
+        skimage.io.imsave(os.path.join(outputDirCrop, imageNameWithoutExtension + "_" + str(index+1) + ".jpg"), crop)
 
 
 if __name__ == '__main__':
@@ -50,8 +98,14 @@ if __name__ == '__main__':
     ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
 
     if args.outputIndexFile:
-        # TODO: Check existence and raise error, because we append
-        pass
+        if os.path.exists(args.outputIndexFile):
+            raise Exception("Index file " + args.outputIndexFile + " already exists")
+
+    if args.outputDirJson:
+        os.makedirs(args.outputDirJson, exist_ok=False)
+
+    if args.outputDirCrop:
+        os.makedirs(args.outputDirCrop, exist_ok=False)
 
     # TODO: Iterate over one level of directories
     subset = ""
@@ -65,18 +119,18 @@ if __name__ == '__main__':
             imageNameWithoutExtension = imageFile[imageFile.rindex('/') + 1:imageFile.rindex('.')]
 
             # HWC image loading
-            img = cv2.imread(imageFile)
-            width = img.shape[1]
-            height = img.shape[0]
+            image = cv2.imread(imageFile)
+            width = image.shape[1]
+            height = image.shape[0]
 
             # resize image, REALLY REALLY IMPORTANT FOR PERFORMANCES
-            img_resized = resizeImage(img, width=width, height=height, maxSide=args.maxSide)
+            image_resized = resizeImage(image, width=width, height=height, maxSide=args.maxSide)
 
-            resized_width = img_resized.shape[1]
-            resized_height = img_resized.shape[0]
+            resized_width = image_resized.shape[1]
+            resized_height = image_resized.shape[0]
 
             # set input image on which we will run segmentation
-            ss.setBaseImage(img_resized)
+            ss.setBaseImage(image_resized)
 
             # Quality switch must executed for each image
             if args.quality:
@@ -90,58 +144,58 @@ if __name__ == '__main__':
             regions = ss.process()
             print('Total Number of Region Proposals: {}'.format(len(regions)))
 
-            # TODO: Manage new outputs format, probably they are the same as before (array of cv2 rects as numpy array)
+            rects_dict_rel = []
+            rects_dict_abs = []
+
+            acceptable_bounding_boxes = 0
+
+            for boundingBox in regions:
+
+                x, y, w, h = boundingBox
+
+                # Check size on resized image
+                if w >= args.minSize and h >= args.minSize:
+
+                    # Pixels are in range [0, width] and [0, height].
+                    # Don't know why last value is added, instead of [0, width - 1] and [0, height - 1]
+                    XMinR = float(x) / float(resized_width)
+                    YMinR = float(y) / float(resized_height)
+                    widthR = float(w) / float(resized_width)
+                    heightR = float(h) / float(resized_height)
+
+                    # Retrieve absolute positions on the original image
+                    x_original = int(x * width / resized_width)
+                    y_original = int(y * height / resized_height)
+                    w_original = int(w * width / resized_width)
+                    h_original = int(h * height / resized_height)
+
+                    this_rect_dict_rel = dict()
+                    this_rect_dict_rel["x_rel"] = XMinR
+                    this_rect_dict_rel["y_rel"] = YMinR
+                    this_rect_dict_rel["width_rel"] = widthR
+                    this_rect_dict_rel["height_rel"] = heightR
+
+                    rects_dict_rel.append(this_rect_dict_rel)
+
+                    this_rect_dict_abs = dict()
+                    this_rect_dict_abs["x"] = x_original
+                    this_rect_dict_abs["y"] = y_original
+                    this_rect_dict_abs["width"] = w_original
+                    this_rect_dict_abs["height"] = h_original
+
+                    rects_dict_abs.append(this_rect_dict_abs)
+
+                    acceptable_bounding_boxes += 1
+                    if acceptable_bounding_boxes >= args.maxRegions:
+                        break
 
             if args.outputIndexFile:
-
-                with open(args.outputIndexFile, 'a') as outputIndexFile:
-
-                    rects_dict = []
-
-                    for boundingBox in regions[:args.maxRegions]:
-
-                        x, y, w, h = boundingBox
-
-                        if w >= args.minSize and h >= args.minSize:
-
-                            this_rect_dict = dict()
-
-                            if args.relativeCoord:
-                                XMinR = float(x) / float(resized_width - 1)
-                                YMinR = float(y) / float(resized_height - 1)
-                                widthR = float(w) / float(resized_width - 1)
-                                heightR = float(h) / float(resized_height - 1)
-
-                                this_rect_dict["x_rel"] = XMinR
-                                this_rect_dict["y_rel"] = YMinR
-                                this_rect_dict["width_rel"] = widthR
-                                this_rect_dict["height_rel"] = heightR
-                            else:
-                                # Retrieve absolute positions on the original image
-                                x_original = int(x * width / resized_width)
-                                y_original = int(y * height / resized_height)
-                                w_original = int(w * width / resized_width)
-                                h_original = int(h * height / resized_height)
-
-                                this_rect_dict["x"] = x_original
-                                this_rect_dict["y"] = y_original
-                                this_rect_dict["width"] = w_original
-                                this_rect_dict["height"] = h_original
-
-                            rects_dict.append(this_rect_dict)
-
-                    rects_dict_string = json.dumps(rects_dict)
-
-                    outputIndexFile.writelines([os.path.join(subset, os.path.basename(imageFile)),
-                                                "\t", rects_dict_string, "\n"])
+                rects_dict = rects_dict_rel if args.relativeCoordIndexFile else rects_dict_abs
+                writeIndexFile(imageRelPath=os.path.join(subset, os.path.basename(imageFile)),
+                               outputIndexFilePath=args.outputIndexFile, rects_dict=rects_dict)
 
             # Proposals export in JSON for our object detection training pipeline
-            if args.outputDirJSON is not None:
-
-                if os.path.exists(args.outputDirJSON) is False:
-                    os.makedirs(args.outputDirJSON)
-
-                #proposals = prepareJSON(width, height, regions, maxRegions=args.maxRegions)
+            if args.outputDirJson:
                 # {
                 # "image": "000001",
                 # "proposals": [
@@ -151,15 +205,12 @@ if __name__ == '__main__':
                 #        "x": 0.0906515568494797,
                 #        "y": 0.0
                 #    },
-                #imageProposal = ImageProposals(imageName=imageNameWithoutExtension, proposals=proposals)
-                #with open(os.path.join(args.outputDirJSON, imageNameWithoutExtension + ".json"), 'w') as outfile:
-                #    json.dump(imageProposal, outfile, default=lambda o: o.__dict__, indent=4, sort_keys=True)
+                prepareJSON(imageRelPath=os.path.join(subset, os.path.basename(imageFile)),
+                            rects_dict=rects_dict_rel, outputDirJson=args.outputDirJson)
 
             # Proposals crops export as images
-            if args.outputDirCrop is not None:
+            if args.outputDirCrop:
 
-                if os.path.exists(args.outputDirCrop) is False:
-                    os.makedirs(args.outputDirCrop)
-
-                #exportCrop(img, regions, args.outputDirCrop, imageNameWithoutExtension,
-                #           minSize=args.minSize, maxRegions=args.maxRegions)
+                exportCrop(imageRelPath=os.path.join(subset, os.path.basename(imageFile)),
+                           image=image, rects_dict=rects_dict_abs,
+                           outputDirCrop=args.outputDirCrop)
